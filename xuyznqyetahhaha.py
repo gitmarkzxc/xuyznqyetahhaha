8cd1a22f9ac32dcc500976b8b0ec1f9029d37351
gitmarkzxc <niki260909@gmail.com>
2024-06-30 01:21:32 +0300

    Add files via upload

---
+++ xuyznqyetahhaha.py
@@ -0,0 +1,12 @@
+api_id = "21480058"
+api_hash = "3c182afdb0a5cf921aa3bda5cafd61af"
+API_TOKEN = "7472310858:AAF-d7RCY0KlmDOVEdDpIyWGC-mHP3QEf4g"
+admin = 1336622678
+updates = -1001616853893
+device_model = "999 SM-G998B"
+system_version = "SDK 31"
+app_version = "8.4.1 (2522)"
+lang_code = "en"
+system_lang_code = "en-US"
+
+SEND_MSG ='@{username} - слитые переписки девушек'
